2650cc07efa594e3f94e5cff8278efaf5ae6c145
James Allen <james@stitchhealth.co>
2022-12-02 09:41:37 +0000

    More declassification of tests

--- test/day_1/test_day_1_solver.py
+++ test/day_1/test_day_1_solver.py
@@ -4,28 +4,25 @@ import pytest as pytest
 from src.day_1 import day_1_solver
 
 
-class TestDay1Part1:
+@pytest.fixture
+def input_truncated() -> list[list[int]]:
+    return [[1000, 2000, 3000]]
 
-    @pytest.fixture
-    def test_input_truncated(self) -> list[list[int]]:
-        return [[1000, 2000, 3000]]
+@pytest.fixture
+def input_long() -> list[list[int]]:
+    return [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
 
-    @pytest.fixture
-    def test_input(self) -> list[list[int]]:
-        return [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
+def test_part_1_returns_6000(input_truncated):
+    expected_result = 6000
+    result = day_1_solver.part_1(input_truncated)
+    assert result == expected_result
 
-    def test_part_1_returns_6000(self, test_input_truncated):
-        expected_result = 6000
-        result = day_1_solver.part_1(test_input_truncated)
-        assert result == expected_result
+def test_part_1_returns_24000(input_long):
+    expected_result = 24000
+    result = day_1_solver.part_1(input_long)
+    assert result == expected_result
 
-    def test_part_1_returns_24000(self, test_input):
-        expected_result = 24000
-        result = day_1_solver.part_1(test_input)
-        assert result == expected_result
-
-    def test_part_2_returns_45000(self, test_input):
-        expected_result = 45000
-        result = day_1_solver.part_2(test_input)
-        assert result == expected_result
-
+def test_part_2_returns_45000(input_long):
+    expected_result = 45000
+    result = day_1_solver.part_2(input_long)
+    assert result == expected_result
