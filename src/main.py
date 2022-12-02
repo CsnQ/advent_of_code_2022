810ccc71d91cc41836cc42fb18b8734f86889d88
James Allen <james@stitchhealth.co>
2022-12-02 09:16:59 +0000

    Declassify inputs

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from src.day_1 import day_1_solver
-from src.utils.text_parser import TextParser
+from src.utils import text_parser
 
 
 def run_puzzles():
@@ -13,7 +13,7 @@ def run_puzzles():
 
 def answer_day_1():
     input_path = Path().cwd() / 'src' / 'input' / 'day_1.txt'
-    data = TextParser().read_list_from_file_as_int_list(input_path)
+    data = text_parser.read_list_from_file_as_int_list(input_path)
     print(day_1_solver.part_1(data))
     print(day_1_solver.part_2(data))
 
